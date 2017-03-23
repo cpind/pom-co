5f331ebd218c6e5e42476091040e0295fa321445
cpind <cyprien.pindat@gmail.com>
2017-03-23 13:48:35 -0400

    moving django project up one level

--- monbeautableau/settings.py
+++ monbeautableau/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print('base_dir: ' + BASE_DIR)
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -128,9 +129,9 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "../src/bootstrap-3.3.7/dist/"),
-    os.path.join(BASE_DIR, "../lib/"),
-    os.path.join(BASE_DIR, "../src"),
+    os.path.join(BASE_DIR, "src/bootstrap-3.3.7/dist/"),
+    os.path.join(BASE_DIR, "lib/"),
+    os.path.join(BASE_DIR, "src"),
 ]
 
 
