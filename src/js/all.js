5c609737968fe88245ebe9587e195837b01b6385
cpind <cyprien.pindat@gmail.com>
2017-04-07 17:25:32 -0400

    missing file

--- src/js/all.js
+++ src/js/all.js
@@ -4,9 +4,9 @@ $(function(){
         headers:{"X-CSRFToken": Cookies.get('csrftoken')}
     });
 
-    var statsmpg = {
-        players:{}
-    };
+    // var statsmpg = {
+    //     players:{}
+    // };
 
     
     var url = window.pomcodata,
@@ -39,19 +39,13 @@ $(function(){
     //initial state
     $('.js-done').hide();
     
-
     $.ajax({
         type: "GET",
         url: url,
         dataType: "text",
         success: function(datacsv) {
-            var data = parseCSV(datacsv);
-            players = data.players;
-            for( var i = 0; i < players.length; ++i ){
-                var p = players[i],
-                    uid = getPlayerUID(p);
-                statsmpg.players[uid] = p;
-            }
+            statsmpg.init(datacsv);
+            players = statsmpg.playersAll;
             dataReadyDef.resolve();
         }
     });
@@ -402,7 +396,7 @@ $(function(){
     
 
     function getPlayerUID(player){
-        return player.nom + '(' + player.team + ')';                              
+        return statsmpg.playerUID(player);
     }
 
 
@@ -639,22 +633,6 @@ $(function(){
                 .attr("opacity", 1)
                 .each(function(d){d._hidden=false;});
             season_on_click(enter.merge(season), click);
-//             if( typeof(click) == "function" ) {
-//                 svg.classed("active", true);
-//                 enter
-//                     .merge(season)
-//                     .on('click', 
-//                         function(d, index, nodes) {
-//                             if( !d.id ) {return;}
-// //                            console.log('clicked : ' + d.id);
-//                             click.call(this, d.id);
-//                         })
-//                     ;
-                
-//             } else {
-//                 svg.classed("active", false);
-//             }
-//            enter.merge(season)
             var records = enter
                 .append("g")
                 .attr('class', 'records')
@@ -848,102 +826,6 @@ $(function(){
         return res;
     }
 
-    //parse the csv file
-    function parseCSV(data){
-
-        var players = [],
-            teams = [],
-            lines = data.split("\n"),
-            current_team = "";
-
-        //extract teams
-        var //reg = new RegExp("-{8}\s\d+\s-\s(\D+)$([^,]+)", "m");
-        reg = new RegExp("-{8}[^0-9]*([0-9]*)[^A-Z]*([A-Z]*).*\n([^,]*)", "mg");
-        data.replace(reg, function(m, sheet, g2, g3){
-            if( sheet == 1) return;
-            teams.push({
-                sheet:sheet,
-                name:g3,
-                short_name:g2
-            });
-        });
-        
-        for(var i = 0; i < lines.length; ++i) {
-            var line = lines[i];
-            
-            if( (new RegExp("-{8}")).test(line)) {
-                current_team = line.match(new RegExp("[A-Z]+"))[0];
-            }
-
-            if( line.startsWith("Poste") ) {
-                extractOpposition(line);
-                continue;
-            }
-            
-            if( ! (new RegExp("^[GDMA],")).test(line)) continue;
-            
-            var split = line.split(",");
-            var notes = parseNotes(line);
-            var player = {
-                poste: split[0],
-                nom: split[1],
-                tit: split[2],
-                entrees: split[3],
-                buts: split[4],
-                team: current_team,
-                notes: notes
-            };
-            
-            players.push(player);
-        }
-
-        return {players:players, teams:teams}
-
-        function parseNotes(line) {
-            var formattedNotes = line.split(','),
-                notes = [];
-            for( var i = 6; i < formattedNotes.length; ++i ) {
-                var formattedNote = split[i],
-                    noteTokens = formattedNote.split(/[\(\)]/),
-                    note = parseFloat(noteTokens[0]),
-                    goals = parseFloat(noteTokens[1]);
-
-                if( isNaN(note) ){
-                    note = 0;
-                }
-                if( isNaN(goals) ) {
-                    goals = 0;
-                }
-                if( noteTokens[0] == "<"){
-                    note = "<";
-                }
-                notes.push({note:note, goals:goals});
-            }
-            return notes;
-        }
-        
-        function extractOpposition(line) {
-            var cells = line.split(','),
-                days = [];            
-            for(var i = 0; i < cells.length; ++i) {
-                var cell = cells[i];
-                if( !(new RegExp("J[0-9]{2}")).test(cell) ) continue;
-                var tokens = cell.split(/[:\ \(\)]/).filter(function(s){return s!== "";});
-                days.push({
-                    day:tokens[0],
-                    location: tokens[1],
-                    opponentTeam: tokens[2]
-                });
-            }
-            for(var i = 0; i < teams.length; ++i){
-                var team = teams[i];
-                if( team.short_name === current_team ) {
-                    team.days = days;
-                }
-            }
-        }
-        
-    }
 
 });
 
